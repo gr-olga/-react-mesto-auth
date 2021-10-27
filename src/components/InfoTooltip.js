cb37b525f0a2a6a6ced2636802b4063ead7585b5
Olga Panfilova <gr-olga@ya.ru>
2021-10-27 13:29:16 +0200

    add info popup

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,16 +1,18 @@
 import React from "react";
+import ok from '../images/ok.png'
 
-function InfoTooltip(){
-    return(
-  <div className="info">
-      <button className="popup__btn-close" type="button"
-              // onClick={closeForm}
-      />
-      <div className="info__box">
-<img src="../images/ok.png"/>
-      <h2>Вы успешно зарегистрировались!</h2>
-      </div>
-  </div>
+function InfoTooltip() {
+    return (
+        <div className="info">
+            <button className="popup__btn-close" type="button"
+                // onClick={closeForm}
+            />
+            <div className="info__box">
+                <img src={ok} className="info__img"/>
+                <h2 className="info__title">Вы успешно зарегистрировались!</h2>
+            </div>
+        </div>
     )
 }
+
 export default InfoTooltip
